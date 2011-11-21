219d1ad8025544f00f111bfaff3a95bd2946e61e
Balazs Nagy <julsevern@gmail.com>
2011-11-21 16:03:18 +0100

    ClearCase: appropriate paths are added instead of explicit settings in aliases.
    Signed-off-by: Balazs Nagy <julsevern@gmail.com>

--- zsh.d/clearcase.zsh
+++ zsh.d/clearcase.zsh
@@ -1,4 +1,7 @@
-alias ct=/usr/atria/bin/cleartool
-alias mt=/usr/atria/bin/multitool
-alias sct="sudo /usr/atria/bin/cleartool"
-alias smt="sudo /usr/atria/bin/multitool"
+pathadd /opt/rational/clearcase/etc/utils
+pathadd /opt/rational/clearcase/bin
+
+alias ct=cleartool
+alias mt=multitool
+alias sct="sudo cleartool"
+alias smt="sudo multitool"
