cde320043dd0b9bcdcbe53b775c47c369fe41dde
Balazs Nagy <julsevern@gmail.com>
2012-01-26 17:28:14 +0100

    Set key bindings for Home/End/Ins/Del/PgUp/PgDn as well as alt-left and alt-right
    Signed-off-by: Balazs Nagy <julsevern@gmail.com>

---
+++ zsh.d/kbd.zsh
@@ -0,0 +1,7 @@
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
+bindkey "\e[5~" beginning-of-history
+bindkey "\e[6~" end-of-history
+bindkey "\e[3~" delete-char
+bindkey "\e\e[C" forward-word
+bindkey "\e\e[D" backward-word
