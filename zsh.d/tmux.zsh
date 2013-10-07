29e1fd0f87ab55388aac84812b9854c7557a6dfa
Balazs Nagy <julsevern@gmail.com>
2013-10-07 13:09:37 +0200

    Change title on logon, and after ssh instead around ssh

--- zsh.d/tmux.zsh
+++ zsh.d/tmux.zsh
@@ -5,8 +5,9 @@ function _set_tmux_title() {
   [[ ${TERM#screen} != $TERM ]] && printf "\033k%s\033\\" ${1:-$(basename $SHELL)}
 }
 
+_set_tmux_title $HOST
+
 function ssh() {
-  _set_tmux_title ${1%%.*}
   command ssh "$@"
-  _set_tmux_title
+  _set_tmux_title $HOST
 }
