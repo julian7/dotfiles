df321ed9ae9e16e3cd8e80e50abf1e7d414c59e9
Balazs Nagy <julsevern@gmail.com>
2013-11-05 12:42:24 +0100

    tmux title should be machine's short name

--- zsh.d/tmux.zsh
+++ zsh.d/tmux.zsh
@@ -5,9 +5,9 @@ function _set_tmux_title() {
   [[ ${TERM#screen} != $TERM ]] && printf "\033k%s\033\\" ${1:-$(basename $SHELL)}
 }
 
-_set_tmux_title $HOST
+_set_tmux_title ${HOST%%.*}
 
 function ssh() {
   command ssh "$@"
-  _set_tmux_title $HOST
+  _set_tmux_title $HOST${HOST%%.*}
 }
