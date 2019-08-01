5fb738a3bf07556b0dfa9d4b35396e7268ea7587
Balazs Nagy <julsevern@gmail.com>
2019-08-01 16:32:57 +0200

    honor brew-ed ruby

--- zsh.d/ruby.zsh
+++ zsh.d/ruby.zsh
@@ -1,3 +1,9 @@
+PATH=`echo $PATH |
+  addpath /usr/local/opt/ruby/bin`
+export PATH
+export LDFLAGS="-L/usr/local/opt/ruby/lib $LDFLAGS"
+export CPPFLAGS="-I/usr/local/opt/ruby/include $CPPFLAGS"
+
 alias b='bundle'
 alias be='bundle exec'
 alias r='bin/rails'
