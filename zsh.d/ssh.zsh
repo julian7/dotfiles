1584c7970089360e1e1b70d43f930d3292441d10
Balazs Nagy <julsevern@gmail.com>
2019-08-03 11:56:27 +0200

    check the right variable for cascading ssh

--- zsh.d/ssh.zsh
+++ zsh.d/ssh.zsh
@@ -1,4 +1,4 @@
-if [[ -z "$SSH_AUTH_SOCK" ]] && whichX gpg-connect-agent; then
+if [[ -z "$SSH_TTY" ]] && whichX gpg-connect-agent; then
     export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
     gpg-connect-agent /bye 2>&1 > /dev/null || echo "Cannot start gpg-agent"
     alias reagent='gpg-connect-agent killagent /bye && gpg-connect-agent /bye'
