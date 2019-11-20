f60d1e58300ae7e15ade4ad240de12e6021968e3
Balazs Nagy <julsevern@gmail.com>
2019-11-20 21:03:59 +0100

    more drastic gpg-agent restarter

--- zsh.d/ssh.zsh
+++ zsh.d/ssh.zsh
@@ -1,5 +1,6 @@
 if [[ -z "$SSH_TTY" ]] && whichX gpg-connect-agent; then
     export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
     gpg-connect-agent /bye 2>&1 > /dev/null || echo "Cannot start gpg-agent"
+    alias killagent='killall -9 gpg-agent && gpg-connect-agent /bye'
     alias reagent='gpg-connect-agent killagent /bye && gpg-connect-agent /bye'
 fi
