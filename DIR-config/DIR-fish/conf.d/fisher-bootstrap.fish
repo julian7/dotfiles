4fdad20a8073ed66b8492e59ca98132eec6e1d54
Balazs Nagy <julsevern@gmail.com>
2020-04-27 23:17:38 +0200

    typo

--- DIR-config/DIR-fish/conf.d/fisher-bootstrap.fish
+++ DIR-config/DIR-fish/conf.d/fisher-bootstrap.fish
@@ -1,6 +1,6 @@
 begin
     set -l fisher $__fish_config_dir/functions/fisher.fish
-    if [ ! $fisher ]
+    if [ ! -e $fisher ]
         curl https://git.io/fisher --create-dirs -sLo $fisher
         source $fisher
         fisher
