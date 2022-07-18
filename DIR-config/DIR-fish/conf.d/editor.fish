3037c07ac88d4209f7767a3c36307136b4c7e683
Balazs Nagy <julsevern@gmail.com>
2022-07-18 11:55:36 +0200

    add new editors: codium, neovide

--- DIR-config/DIR-fish/conf.d/editor.fish
+++ DIR-config/DIR-fish/conf.d/editor.fish
@@ -2,7 +2,7 @@ command -qs joe || alias joe=vim
 
 begin
     set -l editor
-    for editor in "code -w" "subl -w" "mate -w" "nvim" "mvim -f" vim joe vi
+    for editor in "codium -w" "code -w" "subl -w" "mate -w" "neovide" "nvim" "mvim -f" vim joe vi
         if command -qs (string split " " $editor)[1]
             set -x EDITOR "$editor"
             break
