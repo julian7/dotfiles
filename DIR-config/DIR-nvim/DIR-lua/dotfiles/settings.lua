4ae1bc2799fd67bb09260ed479ae94b4505f3f48
Balazs Nagy <julsevern@gmail.com>
2023-04-18 23:04:48 +0200

    nvim / neovide settings idiomatism

--- DIR-config/DIR-nvim/DIR-lua/dotfiles/settings.lua
+++ DIR-config/DIR-nvim/DIR-lua/dotfiles/settings.lua
@@ -3,6 +3,7 @@ vim.cmd.colorscheme('base16-tomorrow-night')
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.guifont = { "JetBrains Mono", ":h13" }
 vim.opt.clipboard = "unnamed"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -10,7 +11,6 @@ vim.opt.cursorline = true
 vim.opt.winwidth = 80
 vim.opt.showtabline = 2
 vim.opt.showmode = false
-vim.opt.pastetoggle = "<leader>2"
 vim.opt.switchbuf = "useopen"
 vim.opt.wrap = false
 vim.opt.autoindent = true
@@ -19,6 +19,38 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.list = true
-vim.opt.listchars = "tab:» ,eol:¬,trail:·,extends:>,precedes:<"
-vim.opt.wildignore = "*.o,*.out,*.obj,*.rbc,*.class,*.svn,*.gem,/vendor/ruby/*,vendor/cache/*,.DS_Store,*.zip,*.tar.gz,*.tar.bz2,*.rar,*.tar.xz,*.tgz,*.tbz2,*.txz,*/vendor/gems/*,*/vendor/cache/*,*/.bundle/*,*/.sass-cache/*,/node_modules/*,*.swp,*~,._*"
-
+vim.opt.listchars = {
+    tab = '» ',
+    eol = '¬',
+    trail = '·',
+    extends = '>',
+    precedes = '<',
+}
+vim.opt.wildignore = {
+    "*.class",
+    "*.gem",
+    "*.o",
+    "*.obj",
+    "*.out",
+    "*.rar",
+    "*.rbc",
+    "*.svn",
+    "*.swp",
+    "*.tar.bz2",
+    "*.tar.gz",
+    "*.tar.xz",
+    "*.tbz2",
+    "*.tgz",
+    "*.txz",
+    "*.zip",
+    "*/.bundle/*",
+    "*/.sass-cache/*",
+    "*/vendor/cache/*",
+    "*/vendor/gems/*",
+    "*~",
+    ".DS_Store",
+    "._*",
+    "/node_modules/*",
+    "/vendor/ruby/*",
+    "vendor/cache/*",
+}
