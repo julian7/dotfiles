81e5dd60ad85761ff5e9491b77e0b24ede125152
Balazs Nagy <julsevern@gmail.com>
2025-11-23 10:07:04 +0100

    fish-native way to set PATH

--- DIR-config/DIR-fish/DIR-conf.d/00-paths.fish
+++ DIR-config/DIR-fish/DIR-conf.d/00-paths.fish
@@ -1,16 +1,3 @@
-set PATH \
-  $HOME/bin \
-  $HOME/.bin \
-  $HOME/.cargo/bin \
-  $HOME/.rbenv/bin \
-  $HOME/.yarn/bin \
-  $HOME/.rd/bin \
-  /opt/homebrew/bin \
-  /usr/local/bin \
-  /usr/local/sbin \
-  /usr/bin \
-  /bin \
-  /usr/sbin \
-  /sbin \
-  /opt/X11/bin \
-  /Applications/Keybase.app/Contents/SharedSupport/bin
+fish_add_path --path /opt/X11/bin /sbin /usr/sbin /bin /usr/bin /usr/local/bin /usr/local/sbin /Applications/Keybase.app/Contents/SharedSupport/bin
+fish_add_path --move --path $HOME/bin $HOME/.bin $HOME/.cargo/bin $HOME/.local/bin $HOME/.rbenv/bin $HOME/.yarn/bin $HOME/.rd/bin /opt/homebrew/bin
+
